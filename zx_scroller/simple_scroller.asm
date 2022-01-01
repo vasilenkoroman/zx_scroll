91caa1f365c732fc5f8b87df721387a50b547048
roman vasilenko <r.vasilenko@gmail.com>
2022-01-01 03:52:21 +0300

    init screen codesize optimization

--- zx_scroller/simple_scroller.asm
+++ zx_scroller/simple_scroller.asm
@@ -28,82 +28,79 @@ upd_de          set 7,d
                 ret z
 next            jr next_line
 
-copy_last_line
-                ld hl, init_screen
-                ld bc,32
-                ldir
-                ld (copy_last_line+1),hl
-                ret            
-
 draw_init_screen
-            ld hl, play_init_screen
-            ld   (#BFBF+1), hl
+                ld hl, play_init_screen
+                ld   (#BFBF+1), hl
    
 
-            LONG_SET_PAGE 7
-            ld (restore_page+1),a
-            ei
+                LONG_SET_PAGE 7
+                ld (restore_page+1),a
+                ei
 
                 ld bc,768
-                ld hl,#4000+6144
-                ld de,#4001+6144
-                ld (hl),#7
+                ld hl,#c000+6144
+                ld de,#4000+6144
                 ldir
 
                 ld bc, 192*256
 screen_loop 
+                ld de,#4000
+                ld hl,#4100
                 bit 0,b
                 jr nz,draw_to_page7
 draw_to_page5
                 ld a, #ba       ; res 7,d
                 ld (upd_de+1),a
-                ld de,#4000
-                ld hl,#c100
+                set 7,h
                 ld a, 7+8
                 jr draw_common
 draw_to_page7
                 ld a, #fa       ; set 7,d
                 ld (upd_de+1),a
-                ld de,#c000
-                ld hl,#4100
+                set 7,d
                 ld a, 7
 draw_common
 
-            push bc
-            ld bc, #7ffd
-            out (c),a
-            ld (restore_page+1),a
+                push bc
+                ld bc, #7ffd
+                out (c),a
+                ld (restore_page+1),a
 
-            call move_screen
-            call copy_last_line
-            pop bc
-            halt
-            djnz screen_loop
+                call move_screen
+copy_last_line
+                ld hl, init_screen
+                ld bc,32
+                ldir
+                ld (copy_last_line+1),hl
 
-            ld bc, #7ffd
-            ld a,8
-            out (c),a
-            ld (restore_page+1),a
+                pop bc
+                halt
+                djnz screen_loop
 
-            ret
+                ld bc, #7ffd
+                ld a,8
+                out (c),a
+                ld (restore_page+1),a
+
+                ret
                          
 play_init_screen
-        push af
-        push de
-        exx
+                push af
+                push de
+                exx
 
-        ld bc, #7ffd
-        ld a,6
-        out (c),a
-        call play
+                ld bc, #7ffd
+                ld a,6
+                out (c),a
+                call play
 
-        ld bc, #7ffd
+                ld bc, #7ffd
 restore_page
-        ld a, 7
-        out (c),a
+                ld a, 7
+                out (c),a
 
-        exx
-        pop de
-        pop af
-        ei
-        ret
+                exx
+                pop de
+                pop af
+                ei
+                ret
