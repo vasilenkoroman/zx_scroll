6d86e7df168c8cff5116af3c63a8950724ec376f
Roman Vasilenko <rvasilenko@networkoptix.com>
2022-01-10 14:53:34 +0300

    get rid of absolute patch in compile_resources.bat

--- zx_scroller/resources/compile_resources.bat
+++ zx_scroller/resources/compile_resources.bat
@@ -1,3 +1,3 @@
-cd c:\zx\scroll\zx_scroller\resources
+cd %~dp0
 encoding_convertor.exe Lapse.z80.asm ../generated_code/reduced_font.asm scroll_text.txt ../generated_code/encoded_text.dat
 scr_deinterlace.exe screen2.scr ../generated_code/screen2.scr
