f9d06c50e8c5775b6261179445c28ed79e70956a
Roman Vasilenko <rvasilenko@networkoptix.com>
2022-01-14 00:13:23 +0300

    update resources

--- zx_scroller/resources/compile_resources.bat
+++ zx_scroller/resources/compile_resources.bat
@@ -4,3 +4,9 @@ scr_deinterlace.exe screen2.scr ../generated_code/screen2.scr
 
 del "../generated_code/final.scr.zx0"
 "../generated_code/zx0.exe" final.scr "../generated_code/final.scr.zx0"
+
+del "../generated_code/screen3.scr.zx0"
+"../generated_code/zx0.exe" screen3.scr "../generated_code/screen3.scr.zx0"
+
+del "../generated_code/scroller.scr.zx0"
+"../generated_code/zx0.exe" scroller.scr "../generated_code/scroller.scr.zx0"
